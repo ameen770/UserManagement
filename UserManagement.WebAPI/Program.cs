b3c5dbc0769dacab62050d340fae1fe66e1adb9b
Abdulrahman-Aldoais <103898762+Abdulrahman-Aldoais@users.noreply.github.com>
2023-12-16 16:51:02 +0300

    command-abdulrman

--- UserManagement.WebAPI/Program.cs
+++ UserManagement.WebAPI/Program.cs
@@ -1,9 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-using UserManagement.Infrastructure.Context;
-using UserManagement.Infrastructure;
-using UserManagement.Application;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
 using System.Globalization;
+using UserManagement.Application;
+using UserManagement.Infrastructure;
+using UserManagement.Infrastructure.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,7 +43,7 @@ builder.Services.Configure<RequestLocalizationOptions>(options =>
 });
 
 var app = builder.Build();
-
+//command
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
